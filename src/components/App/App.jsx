beb99972beb87ce0d67b4944d96da2da9fdfad06
Dmytro Ivashchenko <gen.style22@gmail.com>
2023-08-26 13:08:58 +0300

    Add basic feedback collection

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -11,16 +11,34 @@ export class App extends Component {
     bad: 0,
   };
 
+  incrementFeedback = (category) => {
+    this.setState((state) => {
+      return { [category]: state[category] + 1 };
+    });
+  };
+
   render() {
+    const {
+      good,
+      neutral,
+      bad,
+    } = this.state;
+
     return (<>
       <Wrapper>
         <Title>Feedback collector</Title>
 
         <Subtitle>Please leave a feedback</Subtitle>
-        <Button>Test</Button>
+        <Button onClick={() => this.incrementFeedback('good')}>Good</Button>
+        <Button
+          onClick={() => this.incrementFeedback('neutral')}>Neutral</Button>
+        <Button
+          onClick={() => this.incrementFeedback('bad')}>Bad</Button>
 
         <Subtitle>Statistics</Subtitle>
-        <Counter value={3}>Test</Counter>
+        <Counter value={good}>Good</Counter>
+        <Counter value={neutral}>Neutral</Counter>
+        <Counter value={bad}>Bad</Counter>
       </Wrapper>
 
       <GlobalStyles />
--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,10 +2,14 @@ import React from 'react';
 import { ButtonStyled } from './Button.styled';
 import PropTypes from 'prop-types';
 
-export function Button({ children }) {
-  return (<ButtonStyled>{children}</ButtonStyled>);
+export function Button({
+  children,
+  onClick,
+}) {
+  return (<ButtonStyled onClick={onClick}>{children}</ButtonStyled>);
 }
 
 Button.propTypes = {
   children: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
 };
